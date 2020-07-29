865c5f68140690bf5fa36cbcd138f3b8077c684c
Nikita Zinovev <nrz2000@mail.ru>
2020-07-29 15:07:50 +0300

    Add route for other routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,20 +1,24 @@
-import Vue from 'vue'
-import VueRouter, { RouteConfig } from 'vue-router'
+import Vue from 'vue';
+import VueRouter, { RouteConfig } from 'vue-router';
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
-  const routes: Array<RouteConfig> = [
+const routes: Array<RouteConfig> = [
   {
     path: '/',
     name: 'Services',
-    component: () => import(/* webpackChunkName: "about" */ '../views/Services.vue')
-  }
-]
+    component: () => import(/* webpackChunkName: "about" */ '../views/Services.vue'),
+  },
+  {
+    path: '*',
+    redirect: { name: 'Services' },
+  },
+];
 
 const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
