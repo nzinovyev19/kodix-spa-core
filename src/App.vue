23fde7cdebbe239a390eac1acdd66ae2bce38eec
Nikita Zinovev <nrz2000@mail.ru>
2020-07-29 15:03:58 +0300

    Add global styles

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,14 @@
 <template>
   <v-app>
-
     <v-main>
       <router-view />
     </v-main>
   </v-app>
 </template>
+
+<style lang="sass">
+*
+  box-sizing: border-box
+.full-height
+  height: 100%
+</style>
