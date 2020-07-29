c2c7ea558e8ef522ccaf7af80c9f28ebf083a3e7
Nikita Zinovev <nrz2000@mail.ru>
2020-07-29 15:40:49 +0300

    Add nav drawer

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,50 @@
 <template>
   <v-app>
+    <v-navigation-drawer
+      v-model="drawer"
+      app
+    >
+      <v-list dense>
+        <v-list-item exact :to="{ name: 'Services' }" link>
+          <v-list-item-action>
+            <v-icon>mdi-file-table</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Services</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item :to="{ name: 'Contacts' }" link>
+          <v-list-item-action>
+            <v-icon>mdi-file-table</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Contacts</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <v-app-bar
+      app
+      dark
+    >
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title>kodix-spa-core</v-toolbar-title>
+    </v-app-bar>
     <v-main>
       <router-view />
     </v-main>
   </v-app>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      drawer: null,
+    };
+  },
+};
+</script>
 
 <style lang="sass">
 *
